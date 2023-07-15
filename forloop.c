ed45336334afa4e8e7b6b7a5d73dc53a55196bf7
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-07-15 11:27:32 +0000

    Sat, Jul 15, 2023, 4:27 PM +05:00

---
+++ forloop.c
@@ -0,0 +1,8 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    for (int i = 0; i <3; i+=1) // three arguments. first int i = 0 initiates. Second is a boolean condition i <3 which is being checked. Finally, i+=1 tells loop to increment by one each time the loop runs.
+    printf("meow\n");
+}
--- nestedloop.c
+++
@@ -1,8 +0,0 @@
-#include <cs50.h>
-#include <stdio.h>
-
-int main(void)
-{
-    for (int i = 0; i <3; i+=1)
-    printf("meow\n");
-}
