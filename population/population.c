3cd33c13616593f861486991378b5cc20eb2c358
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-07-30 14:51:55 +0000

    Sun, Jul 30, 2023, 7:51 PM +05:00

---
+++ population/population.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // TODO: Prompt for start size
+    int start_size = get_int("Enter Start Size\n ");
+    
+
+    // TODO: Prompt for end size
+
+    // TODO: Calculate number of years until we reach threshold
+
+    // TODO: Print number of years
+}
