979e43df5e93adfd8eff83bf1697ec12e1bdd787
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-08-12 18:12:46 +0000

    Sat, Aug 12, 2023, 11:12 PM +05:00

--- population/population.c
+++ population/population.c
@@ -4,12 +4,32 @@
 int main(void)
 {
     // TODO: Prompt for start size
-    int start_size = get_int("Enter Start Size\n ");
+    int start_size = get_int("Enter starting population size\n ");
     
 
+    // llmas value should not be less than 9
+    while (start_size < 9)
+    {
+        printf("Population Start size should at least be 9\n");
+        printf(start_size);
+    }
+
     // TODO: Prompt for end size
+    int end_size = get_int("Enter ending population size\n ");
+    if (end_size < start_size)
+    {
+        printf(end_size);
+    }
 
     // TODO: Calculate number of years until we reach threshold
+    // llamas born each year. 
+    start_size = start_size + (start_size/3) - (start_size/4);
+    // truncate 
+
+    // llamas pass away each year
+    int annual_death = start_size/4
+
 
     // TODO: Print number of years
+    printf("Years: ")
 }
