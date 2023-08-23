7ba7d6ac0fd347024abb8e4b5571e0860f5309f6
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-08-23 20:01:38 +0000

    Thu, Aug 24, 2023, 1:01 AM +05:00

--- population/population.c
+++ population/population.c
@@ -30,13 +30,7 @@ int main(void)
     // llamas born each year = n/3
 
     int annual_pop = start_size;
-    // printf("population %i\n"; annual_pop);
-    // int year=0;
-    // while (annual_pop <end_size)
-    // {
-    //  annual_pop = annual_pop + (annual_pop/3) - (start_size/4);
-    //  year++;
-    // }
+
     int year;
     for (year = 0; annual_pop < end_size ; year++)
     {
@@ -48,19 +42,4 @@ int main(void)
    printf("Years: %i\n", year);
 
    return 0;
-
-    // population = year_end
-
-    //for (int year=0; annual_population == end_size ; year++  )
-
-       // annual_population;
-    // truncate
-    //for (year_end == end_size)
-
-    // llamas pass away each year
-    // int annual_death = start_size/4
-
-
-    // TODO: Print number of years
-    // printf("Years: %s\n, year");
 }
