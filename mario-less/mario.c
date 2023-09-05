4cb9c8f6504e27b74b80323856f146a506afbfe8
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-09-05 18:44:04 +0000

    Tue, Sep 5, 2023, 11:44 PM +05:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -4,15 +4,19 @@
 int main(void)
 {
     int input;
-    input = get_int("Height: \n");
 
-    if(input =<1 && input >= 8)
-        {
+    do
+    {
+        input = get_int("Height: \n");
+    }
+
+    while (input =<1 && input >= 8);
+
     //making pyramid with # symbol
     for(int i = 0; i == input; i++)
     {
 
         printf("#\n");
     }
-        }
+        
 }
