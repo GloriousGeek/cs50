25c4235440f75b35ab6ddbf7c81c233696a2b271
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-09-10 18:50:30 +0000

    Sun, Sep 10, 2023, 11:50 PM +05:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -13,14 +13,14 @@ int main(void)
     while (input < 1 || input > 8); //when its true, it would keep prompting the user
 
     //using nested for loop
-    for(int i = 0; i < input; i++) //to print number of rows based on input
+    for(int i = 0; i < input; i++) //to print number of rows (i) based on input
     {
         for (int j = (input - 1) ; j > i; j--) //to give space b/w # based on rows
         {
             printf(" ");
         }
 
-         for (int k = 0; k < i + 1; k++) //to put a hash #
+         for (int k = 0; k < i + 1; k++) //to put a hash #. number of hash is always one more than the row number (1)
         {
             printf("#");
         }
