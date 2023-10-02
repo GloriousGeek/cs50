cc5146affac3203702a79e7a4976a77fce5ecb8c
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-02 17:10:44 +0000

    Mon, Oct 2, 2023, 10:10 PM +05:00

--- hello.c
+++ hello.c
@@ -4,3 +4,12 @@ int main(void)
 {
     printf("hello world\n ");
 }
+
+
+int n;
+
+do
+{
+    n = get_int("Height: \n")
+}
+while (n < 1 || n > 8);
--- mario-less/mario.c
+++ mario-less/mario.c
@@ -5,22 +5,24 @@ int main(void)
 {
     int input;
 
+    // When its true, the do-while loop would keep prompting the user
     do
     {
         input = get_int("Height: \n");
     }
 
-    while (input < 1 || input > 8); //when its true, it would keep prompting the user
+    while (input < 1 || input > 8);
 
-    //using nested for loop
-    for(int i = 0; i < input; i++) //to print number of rows (i) based on input
+    // Using nested for loop to print number of rows (i) based on input
+    for(int i = 0; i < input; i++)
     {
-        for (int j = (input - 1) ; j > i; j--) //to give space b/w # based on rows
+        // Give space b/w # based on rows
+        for (int j = (input - 1) ; j > i; j--)
         {
             printf(" ");
         }
-
-         for (int k = 0; k < i + 1; k++) //to put a hash #. number of hash is always one more than the row number (1)
+        // Putting a hash #. Number of hash is always one more than the row number (i + 1)
+        for (int k = 0; k < i + 1; k++)
         {
             printf("#");
         }
