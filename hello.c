2e553c274965fb01315609724516ff782ace4b67
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-05 11:45:05 +0000

    Thu, Oct 5, 2023, 4:45 PM +05:00

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
+#include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-    printf("hello world\n ");
+    // Getting input and returning it with greeting
+    string answer = get_string("What is your name?\n");
+    printf("hello, %s\n", answer);
 }
