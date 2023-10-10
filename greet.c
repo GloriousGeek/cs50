936fc03c86411b93942d78aa11d5d7dd427f9320
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-10 20:01:32 +0000

    Wed, Oct 11, 2023, 1:01 AM +05:00

--- greet.c
+++ greet.c
@@ -1,11 +1,11 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(int argc, string argv[]);
+int main(int argc, string argv[])
 {
     if (argc == 2)
     {
-        printf("Hello %s \n", argv[1])
+        printf("Hello %s \n", argv[1]);
     }
     else
     {
