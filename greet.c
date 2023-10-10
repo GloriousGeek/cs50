9af8c815b1cf0f267e09c1eecf56f20cbaa55d57
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-10 19:59:16 +0000

    Wed, Oct 11, 2023, 12:59 AM +05:00

--- greet.c
+++ greet.c
@@ -2,7 +2,13 @@
 #include <stdio.h>
 
 int main(int argc, string argv[]);
+{
     if (argc == 2)
     {
-        printf()
+        printf("Hello %s \n", argv[1])
     }
+    else
+    {
+        printf("Hello World\n");
+    }
+}
