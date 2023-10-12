851f6ef3c3ad24152560ab20eb53f1168d3c3d03
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-12 13:55:06 +0000

    Thu, Oct 12, 2023, 6:55 PM +05:00

--- array.c
+++ array.c
@@ -18,7 +18,7 @@ int main(void)
 
     printf("%i\n", dynamic_array[0]);
 
-    for (int i = 1; i < length, i++)
+    for (int i = 1; i < length; i++)
     {
         // Give 2 times the previous element
         dynamic_array[i] = 2 * dynamic_array[i -1];
@@ -26,5 +26,4 @@ int main(void)
         // Printing the current element
         printf("%i\n", dynamic_array[i]);
     }
-
 }
