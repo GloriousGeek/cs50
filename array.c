93b7d0003a987e0fae822de6f345bb854d2374ff
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-12 10:33:36 +0000

    Thu, Oct 12, 2023, 3:33 PM +05:00

--- array.c
+++ array.c
@@ -13,10 +13,13 @@ int main(void)
 
 int dynamic_array[length]
 {
+    // Initial value of array should be 1
+    int dynamic_array[0] = 1;
+
     for (i = 0; i < length, i++)
     {
-       int dynamic_array[0] = 1;
-       
+        printf(")
+
 
     }
 }
