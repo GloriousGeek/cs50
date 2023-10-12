9f72f75b91bc22f2046dda33f022b2de7acdf426
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-12 13:49:28 +0000

    Thu, Oct 12, 2023, 6:49 PM +05:00

--- array.c
+++ array.c
@@ -13,12 +13,12 @@ int main(void)
     // Declare our array
     int dynamic_array[length];
 
-    // Initial value of array should be 1
+    // Set the first value
     dynamic_array[0] = 1;
 
     printf("%i\n", dynamic_array[0]);
 
-    for (i = 0; i < length, i++)
+    for (int i = 0; i < length, i++)
     {
         // Give 2 times the previous value
         dynamic_array[i] = 2 * dynamic_array[i -1];
