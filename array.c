ccc03b08b592c4e7af5cea06cccc7112c50cd293
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-12 13:43:49 +0000

    Thu, Oct 12, 2023, 6:43 PM +05:00

--- array.c
+++ array.c
@@ -15,9 +15,25 @@ int main(void)
     }
     while (length < 1);
 
+    int dynamic_array[];
+    {
+    // Initial value of array should be 1
+    dynamic_array[0] = 1;
+
+    printf("%i\n", dynamic_array[0]);
+
+    for (i = 0; i < length, i++)
+    {
+        // Give 2 times the previous value
+        dynamic_array[i] = 2 * dynamic_array[i -1];
+
+        // Printing the value
+        printf("%i\n", dynamic_array[i]);
+    }
+
     // Calling the function
     int array = dynamic_array[length];
-}
+    }
 
 // Defining the function
 int dynamic_array[]
