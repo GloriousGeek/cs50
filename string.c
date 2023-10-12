01dae45320f149e240346283f3f93b27b01d2e2c
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-12 14:24:47 +0000

    Thu, Oct 12, 2023, 7:24 PM +05:00

--- string.c
+++ string.c
@@ -10,7 +10,7 @@ int main(void)
 
     for (int i = 0; i < length - 1; i++)
     {
-        if name[i] > name[i+1]
+        if (name[i] > name[i+1])
         {
             printf("No\n");
             return 0;
