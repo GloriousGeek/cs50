548cd07aa2c1d8d1cc37dc30766688041efc27e4
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-13 11:05:11 +0000

    Fri, Oct 13, 2023, 4:05 PM +05:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,8 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int count_letters(void);
+
 int main(void)
 {
     // Prompt user for input
     string text = get_string("Text: \n");
+}
+
+int count_letters(void)
+{
+    
 }
