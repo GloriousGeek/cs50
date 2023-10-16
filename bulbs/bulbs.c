2ce75ddb5b18fd71cf7841db0ac8bebf764153b0
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-16 22:05:32 +0000

    Tue, Oct 17, 2023, 3:05 AM +05:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -63,8 +63,9 @@ int string_to_decimal(string message)
 
 void decimal_to_binary(int decimal)
 {
+    
     // Updating the input by diving it to 2 (binary)
-    for (int i = 0; decimal > 0, i++)
+    for (int i = 0; i < decimal, i++)
     {
         decimal /= 2;
         // Using modulus operator
