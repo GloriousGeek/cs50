c90529fb84f52646a286e7b55a7f19ae5bf3ef4a
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-16 20:42:57 +0000

    Tue, Oct 17, 2023, 1:42 AM +05:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -25,7 +25,7 @@ int main(void)
     }
 
     // Print decimal values
-    for (int i = 0; decimal_values[i] != 0; i++)
+    for (int i = 0; decimal_values[i] != '\0'; i++)
     {
         printf("Decimal: %i\n", decimal_values[i]);
     }
