349e22ca68868f00efc00d04d8c9aa3eb47b678c
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-17 22:40:32 +0000

    Wed, Oct 18, 2023, 3:40 AM +05:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -15,32 +15,25 @@ int main(void)
     string message = get_string("Message: ");
 
     // Call string function to get decimal. int* is a pointer not int
+
+    int decimal_values = string_to_decimal(message);
+    printf("decimal values: %i\n", decimal_values);
+
         // Length of string
-        int length = strlen(message);
+        // int length = strlen(message);
 
-        // declaring a variable to use in a for loop and return in the function
-        int decimal_value;
+        // // declaring a variable to use in a for loop and return in the function
+        // int decimal_value;
 
-        for (int i = 0; i < length; i++)
-            {
-                // Updating decimal_value w every iteration
-                decimal_value = message[i];
-                printf("%i\n", decimal_value);
-                printf("%i\n", i);
-            }
+        // for (int i = 0; i < length; i++)
+        //     {
+        //         // Updating decimal_value w every iteration
+        //         decimal_value = message[i];
+        //         printf("%i\n", decimal_value);
+        //         printf("%i\n", i);
+        //     }
 
-
-    // int decimal_values = string_to_decimal(message);
-    // printf("decimal values: %i\n", decimal_values);
-
-    // for NULL - \0
-    // if (decimal_values == '\0')
-    // {
-    //     // 1 means memory allocation failed
-    //     printf("Null\n");
-    // }
-
-    int binary = decimal_to_binary(decimal_value);
+    int binary = decimal_to_binary(decimal_values);
     printf("binary: %i\n", binary);
 }
 
@@ -58,48 +51,39 @@ void print_bulb(int bit)
     }
 }
 
-// int string_to_decimal(string message)
-// {
-//     // Length of string
-//     int length = strlen(message);
+int string_to_decimal(string message)
+{
+    // Length of string
+    int length = strlen(message);
 
-//     // declaring a variable to use in a for loop and return in the function
-//     int decimal_value;
+    // declaring a variable to use in a for loop and return in the function
+    int decimal_value;
 
 
-//     for (int i = 0; i < length; i++)
-//     {
-//         // Updating decimal_value w every iteration
-//         decimal_value = message[i];
-//         printf("%i\n", decimal_value);
-//         printf("%i\n", i);
-//     }
+    for (int i = 0; i < length; i++)
+    {
+        // Updating decimal_value w every iteration
+        decimal_value = message[i];
+        printf("%i\n", decimal_value);
+        printf("%i\n", i);
+    }
 
-//     return decimal_value;
-// }
+    return decimal_value;
+}
 
 int decimal_to_binary(int decimal)
 {
     int binary[BITS_IN_BYTE];
+
     // Updating the input by diving it to 2 (coz its binary)
-    for (int i = 0; decimal > 0; i++)
+    for (int i = 8; decimal > 0; i--)
     {
-        // // Using modulus operator
-        // if (decimal % 2 == 0)
-        // {
-        //     printf("0");
-        // }
-        // else if (decimal % 2 != 0)
-        // {
-        //     printf("1");
-        // }
         binary[i] = decimal % 2;
 
         printf("%i", binary[i]);
 
         // Updating variable
         decimal /= 2;
-
     }
 
     // Neatness
