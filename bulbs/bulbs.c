2252eb86ff198ba87cc5d66cec1174e37c487f3c
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-18 12:36:55 +0000

    Wed, Oct 18, 2023, 5:36 PM +05:00

--- bulbs/bulbs.c
+++ bulbs/bulbs.c
@@ -57,6 +57,7 @@ int string_to_binary(string message)
     for (int i = 0; decimal_value > 0; i++)
     {
         int decimal_value = message[i];
+        
         // Updating binary array with modulous
         binary[i] = decimal_value % 2;
 
