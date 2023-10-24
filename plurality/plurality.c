39c93408f7e14edddca3b57e8301ba0745491d00
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-24 13:19:21 +0000

    Tue, Oct 24, 2023, 6:19 PM +05:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -70,7 +70,7 @@ bool vote(string name)
     {
         if (name == candidates[i].name)
             {
-                candidates[i].vote++;
+                candidates[i].votes++;
                 return true;
             }
 
