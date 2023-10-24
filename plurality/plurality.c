b2f7eef8e47102c854d009a10714be36c3f2f38c
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-24 17:27:06 +0000

    Tue, Oct 24, 2023, 10:27 PM +05:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -93,7 +93,7 @@ void print_winner(void)
             max_candidate = candidates[i];
         }
 
-        printf("%s\n", max_candidate);
+        printf("%s.name\n", max_candidate);
     }
 
     return;
