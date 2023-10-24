9661bc56c6375575bf79ac82e1838cf161711483
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-24 16:59:10 +0000

    Tue, Oct 24, 2023, 9:59 PM +05:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -71,7 +71,7 @@ bool vote(string name)
         if (strcmp(name, candidates[i].name) == 0)
             {
                 candidates[i].votes++;
-                printf("%i", candidates[i].votes);
+                printf("count: %i\n", candidates[i].votes);
                 return true;
             }
     }
