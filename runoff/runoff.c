814a26418beba96300118620c10e3840cc17df3c
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-27 13:40:24 +0000

    Fri, Oct 27, 2023, 6:40 PM +05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -135,7 +135,7 @@ bool vote(int voter, int rank, string name)
         {
             string name[j] = get_string("Rank%i: %s\n", j, name);
 
-            if(strcmp(name, candidate[i].name == 0))
+            if (strcmp(name, candidate[i].name == 0))
             {
                 int preference[i][j];
             }
