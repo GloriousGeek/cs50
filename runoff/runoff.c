6d253eb88bc3e77a0fd4864863c3f1c6442e8d7a
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-10-28 14:15:18 +0000

    Sat, Oct 28, 2023, 7:15 PM +05:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -186,7 +186,11 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    // TODO
+    // Iterate through candidates
+    for (int i = 0; i < candidate_count; i++)
+    {
+
+    }
     return 0;
 }
 
