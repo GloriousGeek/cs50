1efdc10f58c17fb08c10847dac67c0bc23668e1a
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-01 11:56:34 +0000

    Wed, Nov 1, 2023, 4:56 PM +05:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -7,10 +7,11 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            // Check for the white color. All should be zero
-            if (image[i][j].rgbtBlue == 0x00 || image[i][j].rgbtRed == 0x00 || image[i][j].rgbtGreen == 0x00)
+            // Check for the black pixel. All should be zero
+            if (image[i][j].rgbtBlue == 0x00 && image[i][j].rgbtRed == 0x00 && image[i][j].rgbtGreen == 0x00)
             {
-                image[i][j].rgbtGreen == 0xFF
+                // Turn black pixel to Green
+                image[i][j].rgbtGreen == 0xFF;
             }
         }
     }
