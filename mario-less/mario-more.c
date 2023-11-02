9ed5d9cfa32fda4bbad24922a5f60d41a0b36025
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-02 19:27:36 +0000

    Fri, Nov 3, 2023, 12:27 AM +05:00

---
+++ mario-less/mario-more.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user for the height of the pyramid
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height < 1 || height > 8);
+
+    // Build the pyramid
+    for (int i = 0; i < height; i++)
+    {
+        // Print hashes for left-aligned side
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        // Add space between the two sides
+        printf("  ");
+
+        // Print hashes for right-aligned side
+        for (int j = 0; j <= i; j++)
+        {
+            printf("#");
+        }
+
+        // Newline to move to the next row
+        printf("\n");
+    }
+
+    return 0;
+}
