e659a3bb663cab86de8ce65219658bb0130bebae
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-02 18:50:42 +0000

    Thu, Nov 2, 2023, 11:50 PM +05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,7 +25,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-            sepiaRed = .393 * image[i][j].rgbtRed + .769 * originalGreen + .189 * originalBlue
+            BYTE sepiaRed = .393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue
         }
     }
     return;
