01f5c06083191b1d5d597606320aecc6fd441533
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-02 19:14:33 +0000

    Fri, Nov 3, 2023, 12:14 AM +05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -25,7 +25,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width; j++)
         {
-
+            // Rounding to nearest integer
             BYTE sepiaRed = round(.393 * image[i][j].rgbtRed + .769 * image[i][j].rgbtGreen + .189 * image[i][j].rgbtBlue);
 
             BYTE sepiaGreen = round(.349 * image[i][j].rgbtRed + .686 * image[i][j].rgbtGreen + .168 * image[i][j].rgbtBlue);
