552771daab51c53db3362c9fec8b4e0e084cd9e1
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-03 20:20:14 +0000

    Sat, Nov 4, 2023, 1:20 AM +05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -112,7 +112,9 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
                     RGBTRIPLE neighbor_x = i + 1;
                     RGBTRIPLE neighbor_y = j + 1;
                     if (neighbor_x >= 0 && neighbor_x < width && neighbor_y >= 0 && neighbor_y < height)
-                    
+                    {
+                        
+                    }
                 }
             }
             RGBTRIPLE new_value =
