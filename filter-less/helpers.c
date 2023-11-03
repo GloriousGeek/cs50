85c40b8bf9f2758f5d1114b85a43e52a1a858bba
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-03 10:44:39 +0000

    Fri, Nov 3, 2023, 3:44 PM +05:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -98,5 +98,13 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    // Iterating through height/rows
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            
+        }
+    }
     return;
 }
