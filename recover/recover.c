831642fdc90342a827c3876ae06e04388c6b179f
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-06 17:00:08 +0000

    Mon, Nov 6, 2023, 10:00 PM +05:00

--- recover/recover.c
+++ recover/recover.c
@@ -19,7 +19,11 @@ int main(int argc, char *argv[])
 
     // Read data from memory card into buffer in 512 byte chunks
     unsigned char buffer[512];
-    
+
+    // Initialize variables for output file and counter
+    FILE *output = NULL;
+    int jpeg_count = 0;
+
     fread(storedata[], 512, 512, FILE *file)
 
     // Iterate over memory card's data
