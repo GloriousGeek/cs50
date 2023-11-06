f1bfcdc56d3068c5f9e2c5d694ddcb82b0f2bd72
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-06 19:17:50 +0000

    Tue, Nov 7, 2023, 12:17 AM +05:00

--- recover/recover.c
+++ recover/recover.c
@@ -34,7 +34,7 @@ int main(int argc, char *argv[])
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] & 0xf0 == 0xe0)
         {
             // Create new file
-            
+            char new_file[8];
             fwrite()
         }
     }
