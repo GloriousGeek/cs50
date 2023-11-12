02eb2314bd46698ab82e44620d89578a9bfda110
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-12 20:31:18 +0000

    Mon, Nov 13, 2023, 1:31 AM +05:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -101,8 +101,8 @@ void free_family(person *p)
     }
 
     // TODO: Free parents recursively
-    free(free_family(p.parents[0]));
-    free(free_family(p.parents[1]));
+    free_family(p.parents[0]);
+    free_family(p.parents[1]);
 
     // TODO: Free child
     free(p);
