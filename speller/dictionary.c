41d34c7ebaaf19a10878268d0015e508923e5081
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-13 20:18:42 +0000

    Tue, Nov 14, 2023, 1:18 AM +05:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -80,7 +80,7 @@ bool load(const char *dictionary)
 
         // Insert that word into the linked list
         // Add a new node to a linked list
-        node *head = 
+        node *head = table[h];
 
 
     }
