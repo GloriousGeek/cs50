6ac841194b0c4c9d940564968581893086011161
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-16 20:30:49 +0000

    Fri, Nov 17, 2023, 1:30 AM +05:00

--- hello.py
+++ hello.py
@@ -5,3 +5,4 @@ import cs50
 answer = get_string("What's your name?")
 print("hello, " + answer) # + Concatinating (joining) strings
 print("hello,", answer) # Another way
+print(f"hello, {answer}") # Best way
