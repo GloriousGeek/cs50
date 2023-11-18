6107bf2db2c31a29e67fa4045ccc5171e265c35c
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-18 22:29:15 +0000

    Sun, Nov 19, 2023, 3:29 AM +05:00

--- python/world-cup/tournament.py
+++ python/world-cup/tournament.py
@@ -63,7 +63,7 @@ def simulate_round(teams):
 def simulate_tournament(teams):
     """Simulate a tournament. Return name of winning team."""
     # List of teams as input
-    input = input()
+    simulation = simulate_round()
 
 
 
