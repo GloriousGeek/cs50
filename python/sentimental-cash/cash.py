918fbdcc3b6c375f3f36c764d03e1bf53830d2fa
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-19 20:39:03 +0000

    Mon, Nov 20, 2023, 1:39 AM +05:00

--- python/sentimental-cash/cash.py
+++ python/sentimental-cash/cash.py
@@ -1,8 +1,11 @@
 from cs50 import get_float
 
-try:
-    user_input = get_float(input("Change owed: "))
-except ValueError:
-    print("Enter a valid digit")
+while True:
+    try:
+        user_input = get_float(input("Change owed: "))
+        if user_input > 0:
+            return user_input
+    except ValueError:
+        print("Enter a valid digit")
 
 
