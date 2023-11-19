58cc9ed1cd05ebd834244096a3a56d76db8ab486
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-19 11:37:01 +0000

    Sun, Nov 19, 2023, 4:37 PM +05:00

--- python/world-cup/tournament.py
+++ python/world-cup/tournament.py
@@ -32,7 +32,8 @@ def main():
 
     counts = {}
     # TODO: Simulate N tournaments and keep track of win counts
-    
+    for teams in N:
+        
 
     # Print each team's chances of winning, according to simulation
     for team in sorted(counts, key=lambda team: counts[team], reverse=True):
