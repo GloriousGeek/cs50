766ec3235210b863883ab4e8dbcf9445b2728edc
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-19 20:39:46 +0000

    Mon, Nov 20, 2023, 1:39 AM +05:00

--- python/sentimental-cash/cash.py
+++ python/sentimental-cash/cash.py
@@ -4,7 +4,7 @@ while True:
     try:
         user_input = get_float(input("Change owed: "))
         if user_input > 0:
-            return user_input
+            break
     except ValueError:
         print("Enter a valid digit")
 
