70b374a0e4be5dc6db61e5ab1385074699b064de
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-19 13:29:35 +0000

    Sun, Nov 19, 2023, 6:29 PM +05:00

--- python/sentimental-mario-less/mario.py
+++ python/sentimental-mario-less/mario.py
@@ -2,9 +2,9 @@ height = get_height()
 
 def main():
     for i in range(height):
-    for j in range(height):
-        print(" " , end ="")
-    print("#")
+        for j in range(height):
+            print(" " , end ="")
+        print("#")
     # Decreasing the user_input by 1
     # user_input -= 1
 
@@ -16,6 +16,7 @@ def get_height():
             return n
 
 
+get_height()
 main()
 
 
