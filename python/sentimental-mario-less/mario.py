279012f48d14671ebf0e361664218505d1b114bd
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-19 12:58:38 +0000

    Sun, Nov 19, 2023, 5:58 PM +05:00

--- python/sentimental-mario-less/mario.py
+++ python/sentimental-mario-less/mario.py
@@ -4,7 +4,7 @@ user_input = int(input("Height: "))
 while (8 < user_input < 1):
     print(user_input)
 
-for i in range(user_input, -1):
+for i in range(user_input, 0, -1):
     print(" " * (user_input - 1), end ="")
     for j in range(user_input):
         print("#")
