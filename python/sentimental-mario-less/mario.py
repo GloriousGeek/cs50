1fba91c780d658c452d4d187eb8bc9071f9a7141
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-19 12:47:51 +0000

    Sun, Nov 19, 2023, 5:47 PM +05:00

--- python/sentimental-mario-less/mario.py
+++ python/sentimental-mario-less/mario.py
@@ -1,6 +1,11 @@
 user_input = int(input("Height: "))
 
-# Re-prompt the user b/w 1 and 8
+# Re-prompt the user for value b/w 1 and 8
 while (8 < user_input < 1):
     print(user_input)
 
+for i in range(user_input):
+    print()
+    for j in range(user_input):
+
+    print(" ")
