1ce7c4499f39bef774d4ddffe7f4b7cbf2c166a6
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-19 22:07:24 +0000

    Mon, Nov 20, 2023, 3:07 AM +05:00

--- python/sentimental-cash/cash.py
+++ python/sentimental-cash/cash.py
@@ -1,19 +1,48 @@
 from cs50 import get_float
 
-while True:
-    try:
-        get_dollars = get_float(input("Change owed: "))
-        if user_input > 0:
-            break
-        else:
-            print("Enter a positive value")
-    except ValueError:
-        print("Enter a valid digit")
+def main():
+
+
+
+def get_dollars():
+    while True:
+        try:
+            get_dollars = get_float(input("Change owed: "))
+            if user_input > 0:
+                break
+            else:
+                print("Enter a positive value")
+        except ValueError:
+            print("Enter a valid digit")
 
 def quarters(dollars):
-    for i in dollars:
-        if dollars >= 0.25:
-            i += 1
-        return i
+    # Initializing a variable to count quarters
+    q = 0
+    while dollars >= 25:
+        q += 1
+        dollars -= 0.25
+    return q
 
 
+def dimes(dollars):
+    d = 0
+    while dollars >= 10:
+        d += 1
+    return d
+
+
+def nickels(dollars):
+    n = 0
+    while dollars >= 5:
+        n += 1
+    return n
+
+
+def pennies(dollars):
+    p = 0
+    while dollars >= 1:
+        p += 1
+    return p
+
+
+main()
