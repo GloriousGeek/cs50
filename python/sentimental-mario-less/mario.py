70460f77429d25e85113f4cbefdd3f7ccf1ab8d2
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-19 17:31:37 +0000

    Sun, Nov 19, 2023, 10:31 PM +05:00

--- python/sentimental-mario-less/mario.py
+++ python/sentimental-mario-less/mario.py
@@ -1,5 +1,4 @@
 def main():
-
     # Re-prompt the user for value b/w 1 and 8
     while True:
         height = int(input("Height: "))
@@ -9,23 +8,16 @@ def main():
         else:
             print("Enter height between 1 and 8")
 
-
-
     # Print the pyramid
     for i in range(1, height + 1):
         # Print spaces
         for j in range(height - i):
-            print(" ", end = "")
+            print(" ", end="")
         # Print hash
         for k in range(i):
-            print("#", end= "")
+            print("#", end="")
         # Move to the next line after each row
         print()
 
+
 main()
-
-
-
-
-
-
