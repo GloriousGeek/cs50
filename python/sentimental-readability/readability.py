3aa285d4719f82ae24ad07c34891d07ad08326dc
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-20 12:00:51 +0000

    Mon, Nov 20, 2023, 5:00 PM +05:00

--- python/sentimental-readability/readability.py
+++ python/sentimental-readability/readability.py
@@ -5,6 +5,7 @@ text = get_string("Text: ")
 
 # Count letters
 letter_count = len(text)
+print(letter_count)
 
 # Word count
 def count_words(text):
@@ -17,14 +18,17 @@ def count_words(text):
 
 
 def count_sentences(text):
-    #str.count() counts the number of occurances of 
+    # The str.count() method returns the number of times a specified value appears in the string
+    return (text.count(".") + text.count("!") + text.count("?"))
 
-    sentence_count = 0
-    for i in text:
-    if (text == "." or text == "!" or text == "?"):
-        # Update count
-        sentence_count += 1
-    return sentence_count
+    # sentence_count = 0
+    # for i in text:
+    # if (text == "." or text == "!" or text == "?"):
+    #     # Update count
+    #     sentence_count += 1
+    # return sentence_count
+
+
 
 
 
