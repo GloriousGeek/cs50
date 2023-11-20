c36db59357981be3143629b02b77d6fc099f1704
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-20 20:34:15 +0000

    Tue, Nov 21, 2023, 1:34 AM +05:00

--- python/dna/dna.py
+++ python/dna/dna.py
@@ -24,10 +24,8 @@ def main():
 
         # Store values in database variable
         for row in reader:
-            # Accessing the "name" column
-            name = row["name"]
-
-            database.append({i})
+            # Add to database list
+            database.append(row)
     print(reader)
 
     # TODO: Read DNA sequence file into a variable
