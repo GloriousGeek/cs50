e1e54a65100e0d90963e36d4c7c73dbcc0fda127
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-20 19:56:12 +0000

    Tue, Nov 21, 2023, 12:56 AM +05:00

--- python/dna/dna.py
+++ python/dna/dna.py
@@ -5,7 +5,7 @@ import sys
 def main():
 
     # TODO: Check for command-line usage
-    if len(argv) != 3:
+    if len(sys.argv) != 3:
         print("Missing command-line arguments")
         sys.exit(1)
 
