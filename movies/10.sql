417ef43cb940b339c7f75062dc3225f6f14039c5
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-26 21:33:01 +0000

    Mon, Nov 27, 2023, 2:33 AM +05:00

--- movies/10.sql
+++ movies/10.sql
@@ -1,4 +1,4 @@
-SELECT people.name FROM people
+SELECT people.name, ratings.rating FROM people
 WHERE people.id IN (SELECT person_id FROM directors
 JOIN ratings ON ratings.movie_id = directors.movie_id
 WHERE rating >= 9.0);
