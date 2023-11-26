f66fd3d464eb22de3b9c3317ce5db6ecfa76206f
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-26 22:04:19 +0000

    Mon, Nov 27, 2023, 3:04 AM +05:00

--- movies/11.sql
+++ movies/11.sql
@@ -2,4 +2,6 @@ SELECT movies.title FROM movies
 JOIN stars ON stars.movie_id = movies.id
 JOIN ratings ON ratings.movie_id = stars.movie_id
 JOIN people ON people.id = stars.person_id
-WHERE people.name = 'Chadwick Boseman' ORDER BY ratings.rating DESC LIMIT 5;
+WHERE people.name = 'Chadwick Boseman'
+ORDER BY ratings.rating DESC
+LIMIT 5;
