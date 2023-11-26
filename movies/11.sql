b2bb5452c10ebc0058394f6c637ed3b73e1f334e
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-26 21:50:51 +0000

    Mon, Nov 27, 2023, 2:50 AM +05:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,4 +1,4 @@
-SELECT movies.title FROM movies
+SELECT movies.title, ratings.rating, people.name FROM movies
 JOIN stars ON stars.movie_id = movies.id
 JOIN ratings ON ratings.movie_id = stars.movie_id
 JOIN people ON people.id = stars.person_id
