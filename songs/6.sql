721e8381ec2de050f13cd725b2a6b3b2ca3679b5
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-26 13:44:17 +0000

    Sun, Nov 26, 2023, 6:44 PM +05:00

--- songs/6.sql
+++ songs/6.sql
@@ -1,6 +1,6 @@
 SELECT id FROM artists WHERE name == 'Post Malone';
 SELECT name FROM songs WHERE artist_id == 54;
 
-SELECT name FROM songs
+SELECT songs.name FROM songs
 JOIN artists ON songs.artist_id = artists.id
 WHERE artists.name == 'Post Malone';
