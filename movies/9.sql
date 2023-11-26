e7e6b4d59452da5735434ccac4049a3cf890f0a1
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-26 21:27:31 +0000

    Mon, Nov 27, 2023, 2:27 AM +05:00

--- movies/9.sql
+++ movies/9.sql
@@ -1,4 +1,4 @@
-SELECT people.name, people.birth FROM people
+SELECT people.name FROM people
 WHERE people.id IN (SELECT person_id FROM stars
 JOIN movies ON stars.movie_id = movies.id
 WHERE movies.year == 2004) ORDER BY people.birth;
