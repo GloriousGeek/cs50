a888bbf864fa6c67c9ab577ddce7ac8158e4bcc6
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-27 14:46:20 +0000

    Mon, Nov 27, 2023, 7:46 PM +05:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,2 +1,4 @@
 SELECT movies.title FROM movies
-JOIN stars ON 
+JOIN stars ON stars.movie_id = movies.id
+JOIN people ON people.id = stars.person_id
+WHERE people.name = 'Bradley Cooper' AND people.name = 'Jennifer Lawrence';
