208336a4efc3e66db70eebdd54641993f0a4dd92
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-27 20:06:27 +0000

    Tue, Nov 28, 2023, 1:06 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,2 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+SELECT * FROM crime_scene_reports
+WHERE day = 28 AND month = 7 AND year = 2021 AND street = 'Humphrey Street';
