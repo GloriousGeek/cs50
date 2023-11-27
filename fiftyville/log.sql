3710e9792f93db621d806d9ce9ecc5431635e72f
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-27 20:57:00 +0000

    Tue, Nov 28, 2023, 1:57 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -23,7 +23,11 @@ WHERE atm_location = 'Leggett Street' AND day = 28 AND month = 7 AND year = 2021
 
 -- We got account_number of 8 people. Lets query the bank_accounts and link it to people
 SELECT * FROM bank_accounts
-WHERE account_number IN (SELECT account_number FROM atm_transactions
-WHERE atm_location = 'Leggett Street'
-AND day = 28 AND month = 7 AND year = 2021 AND transaction_type = 'withdraw')
-
+WHERE account_number IN
+    (SELECT account_number FROM atm_transactions
+    WHERE atm_location = 'Leggett Street'
+    AND day = 28 AND month = 7 AND year = 2021 AND transaction_type = 'withdraw')
+    WHERE person_id IN
+        (SELECT id FROM people
+        WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs
+        WHERE hour = 10 AND day = 28 AND month = 7 AND year = 2021 AND activity = 'exit'));
