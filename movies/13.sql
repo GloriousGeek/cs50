04d4905cc914e8505aec309ffc650ef084df42c3
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-27 15:28:07 +0000

    Mon, Nov 27, 2023, 8:28 PM +05:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,4 +1,4 @@
 SELECT people.name FROM people
 JOIN stars ON stars.person_id = people.id
 JOIN movies ON movies.id = stars.movie_id
-WHERE people.name = 'Kevin Bacon'
+WHERE people.name = 'Kevin Bacon' AND people.birth = 1958;
