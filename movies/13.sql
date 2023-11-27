ae8e93a1159ddd9f80f1cf9cc0aa15f37eec4c20
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-27 15:55:01 +0000

    Mon, Nov 27, 2023, 8:55 PM +05:00

--- movies/13.sql
+++ movies/13.sql
@@ -1,8 +1,17 @@
-SELECT DISTINCT people.name FROM people
-JOIN stars ON stars.person_id = people.id
-JOIN movies ON movies.id = stars.movie_id
-WHERE movies.id IN (SELECT movies.id FROM stars
-JOIN people ON stars.person_id = people.id
-WHERE people.name = 'Kevin Bacon' AND people.birth = 1958)
-AND people.name != 'Kevin Bacon';
+-- SELECT DISTINCT people.name FROM people
+-- JOIN stars ON stars.person_id = people.id
+-- JOIN movies ON movies.id = stars.movie_id
+-- WHERE movies.id IN (SELECT movies.id FROM stars
+-- JOIN people ON stars.person_id = people.id
+-- WHERE people.name = 'Kevin Bacon' AND people.birth = 1958)
+-- AND people.name != 'Kevin Bacon';
 
+
+SELECT DISTINCT p.name
+FROM people p
+JOIN stars s ON p.id = s.person_id
+JOIN movies m ON s.movie_id = m.id
+JOIN stars k ON m.id = k.movie_id
+JOIN people kb ON k.person_id = kb.id
+WHERE kb.name = 'Kevin Bacon' AND kb.birth_year = 1958
+  AND p.name != 'Kevin Bacon';
