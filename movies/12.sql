1ed6ab71cd8ead36b3909e7729459dde35621b9c
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-27 15:04:56 +0000

    Mon, Nov 27, 2023, 8:04 PM +05:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,4 +1,7 @@
 SELECT movies.title FROM movies
 JOIN stars ON stars.movie_id = movies.id
-JOIN people ON people.id = stars.person_id
-AND (people.name = 'Bradley Cooper' AND people.name = 'Jennifer Lawrence');
+JOIN people ON people.id = stars.person_id AND people.name = 'Bradley Cooper'
+JOIN stars AS s2 ON s2.movie_id = movies.id
+JOIN people AS p2 ON p2.id = s2.person_id AND p2.name = 'Jennifer Lawrence';
+
+AND people.name IN ('Bradley Cooper', 'Jennifer Lawrence')
