2f4829b020e99a42ba9a88207c3b2aa2c2e717a4
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-27 21:14:05 +0000

    Tue, Nov 28, 2023, 2:14 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -43,3 +43,4 @@ WHERE account_number IN
         (SELECT id FROM people
         WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs
         WHERE hour = 10 AND day = 28 AND month = 7 AND year = 2021 AND activity = 'exit')));
+
