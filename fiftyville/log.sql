7ff87960fd5d378162eb9c5d2489ff35060ebeed
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-11-27 20:17:30 +0000

    Tue, Nov 28, 2023, 1:17 AM +05:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -4,5 +4,4 @@
 SELECT * FROM crime_scene_reports
 WHERE day = 28 AND month = 7 AND year = 2021 AND street = 'Humphrey Street';
 
-SELECT * FROM crime_scene_reports
-WHERE day = 28 AND month = 7 AND year = 2021 AND street = 'Humphrey Street';
+SELECT * FROM interviews
