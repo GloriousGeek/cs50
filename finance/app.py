166f8fbef7a58a16e948b4f4b6fd76d21e31f372
GloriousGeek <103073645+GloriousGeek@users.noreply.github.com>
2023-12-15 21:10:11 +0000

    Sat, Dec 16, 2023, 2:10 AM +05:00

--- finance/app.py
+++ finance/app.py
@@ -5,8 +5,7 @@ from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from helpers import apology, login_required, lookup, usd, is_valid_password
-
+from helpers import apology, login_required, lookup, usd, is_valid_passworda
 # Configure application
 app = Flask(__name__)
 
